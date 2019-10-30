5331f7a1c384b896a93a6b817086b86bfb0c9f20
ura <ura@example.com>
2019-10-30 15:46:36 +0900

    Develop viewAllCoupons Done

--- amiCoupon/ViewController.swift
+++ amiCoupon/ViewController.swift
@@ -11,21 +11,14 @@ import UIKit
 class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
     @IBOutlet weak var tableView: UITableView!
     
-    var couponBenefit: String = "" {
+    var coupons: [[String: Any]] = [] { //パースした[String: Any]型のクーポンデータを格納するメンバ変数
         didSet{
             tableView.reloadData()
         }
     }
-    var couponDeadline: String = "" {
-        didSet{
-            tableView.reloadData()
-        }
-    }
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
 
         let url: URL = URL(string: "http://127.0.0.1:8000/coupon/")!
         let task: URLSessionTask = URLSession.shared.dataTask(with: url, completionHandler: {(data, response, error) in
@@ -34,13 +27,9 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
                 let couponData = couponDataArray.map { (couponData) -> [String: Any] in
                     return couponData as! [String: Any]
                 }
-                print(couponData[0]["coupon_benefits"] as! String)
-                print(couponData[1]["coupon_benefits"] as! String)
-
                 
                 DispatchQueue.main.async() { () -> Void in
-//                    self.couponBenefit = couponData["coupon_benefits"] as! String
-//                    self.couponDeadline = couponData["coupon_deadline"] as! String
+                    self.coupons = couponData
                     }
                 }
             catch {
@@ -52,16 +41,17 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return self.coupons.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         //セルを作る
         let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "couponCell")
+        let coupon = self.coupons[indexPath.row]
         //テキストにクーポン特典を設定
-        cell.textLabel?.text = self.couponBenefit
-        //サブテキストにクーポンの有効期限を設定
-        cell.detailTextLabel?.text = "有効期限：" + self.couponDeadline
+        cell.textLabel?.text = (coupon["coupon_benefits"] as! String)
+         //サブテキストにクーポンの有効期限を設定
+        cell.detailTextLabel?.text = "有効期限：" + (coupon["coupon_deadline"] as! String)
 
         return cell
     }
